61d9c5b6097959e382bcaacb74e05a0050cad677
Carlos Coppa <Carlos Coppa>
2022-04-21 12:40:49 -0300

    Day 34 Parcial

---
+++ Day_34/ui.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        self.window.mainloop()
+    
